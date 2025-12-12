b9d9b6ea39bbe26a25d3183e6b33df58c47d93de
AlphaBotZ <zengteng@163.com>
2025-12-12 10:47:46 +0800

    Update diy-part1.sh

--- openwrt/diy-part1.sh
+++ openwrt/diy-part1.sh
@@ -20,5 +20,3 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
