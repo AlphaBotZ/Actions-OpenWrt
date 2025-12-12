8a2a2ccc535fec742287375192e143ba39d3c78f
AlphaBotZ <zengteng@163.com>
2025-12-12 17:47:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -45,6 +45,7 @@ git clone https://github.com/sbwml/luci-app-openlist2 package/openlist
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 
 # 集成自定义 LuCI 包
+mkdir -p openwrt/package
 [ -d "$GITHUB_WORKSPACE/luci-app-github-update" ] && \
 mv $GITHUB_WORKSPACE/luci-app-github-update openwrt/package/
 echo "CONFIG_PACKAGE_luci-app-github-update=y" >> openwrt/.config
